2e58c0a6653234354043379535535119406aa318
yomna99 <yomna.hajji@supcom.tn>
2023-02-08 20:09:57 +0100

    first_commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CalendarComponent } from './core/calendar/calendar.component';
+import { HomeComponent } from './core/home/home.component';
+import { AdminComponent } from './dashboard/admin/admin.component';
+import { ContactComponent } from './dashboard/contact/contact.component';
+import { ProfilComponent } from './dashboard/profil/profil.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'profil', component: ProfilComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
